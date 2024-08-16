cd3242fadc9197b487c619c1cf0b522e50537a65
jeanlin <jeanlin1203@gmail.com>
2024-08-16 01:20:33 -0400

    Update contents in the landing page

--- js/typing_animation.js
+++ js/typing_animation.js
@@ -1,62 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
     const texts = [
-        "Hi, I am Yu-Chen Lin ",
-        "I am a Master's student in Computer Science and Engineering at The Ohio State University ",
-        "I specialize in software engineering, robotic algorithm development, system performance optimization, and full-stack development ",
-        "Welcome to explore my work and achievements "
+        "Hi, I am Yu-Chen Lin.",
+        "I am a Master's student in Computer Science and Engineering at The Ohio State University.",
+        "I specialize in software engineering, robotic algorithm development, ",
+        "system performance optimization, and full-stack development.",
+        "Welcome to explore my work and achievements! "
     ];
 
-    const colors = [
-        "#FFFFFF"
-    ];
 
     const textContainer = document.getElementById("dynamicText");
     let textIndex = 0;
-    let colorIndex = 0;
-    let wordIndex = 0;
+    let wordIndex = 1;
     let currentText = "";
-    let isDeleting = false;
     let isCompleted = false;
 
     function typeText() {
         if (textIndex < texts.length) {
             if (!isCompleted && wordIndex < texts[textIndex].length) {
                 // Add the next character
-                currentText = texts[textIndex].substring(0, wordIndex);
+                currentText = texts[textIndex].substring(wordIndex - 1, wordIndex);
                 wordIndex++;
-                // textContainer.innerHTML = `<span>${currentText}</span>`;
-                textContainer.innerHTML = `<span style="color: ${colors[colorIndex]}">${currentText}</span>`;
-            } else if (isCompleted) {
-                textIndex++; // Move to the next word
-                wordIndex = 0;
+                textContainer.innerHTML += `<span style="color: #FFFFFF">${currentText}</span>`;
             }
-            // else if (isDeleting && wordIndex >= 0) {
-            //     // Remove the last character
-            //     currentText = texts[textIndex].substring(0, wordIndex);
-            //     wordIndex--;
-            //     // textContainer.innerHTML = `<span>${currentText}</span>`;
-            //     textContainer.innerHTML = `<span style="color: ${colors[colorIndex]}">${currentText}</span>`;
-            // }
+            if (isCompleted) {
+                textIndex++; // Move to the next word
+                textContainer.innerHTML += `<br>`;
+                wordIndex = 0;
+                isCompleted = false;
+            }
 
             if (wordIndex === texts[textIndex].length) {
                 isCompleted = true;
             }
-            // // If the word is complete, wait and then start deleting
-            // if (wordIndex >= texts[textIndex].length) {
-            //     isDeleting = true;
-            //     setTimeout(typeText, 2000); // Pause before deleting
-            // } else if (isDeleting && wordIndex === 0) {
-            //     isDeleting = false;
-            //     textIndex++; // Move to the next word
-            //     setTimeout(typeText, 500); // Pause before typing the next word
-            // } else {
-            //     setTimeout(typeText, isDeleting ? 50 : 100); // Speed up deletion, slow down typing
-            // }
+            setTimeout(typeText, 50); // Pause before typing the next word
         }
-        // else {
-        //     textIndex = 0; // Loop back to the first text
-        //     setTimeout(typeText, 500);
-        // }
     }
 
     typeText(); // Start the typing effect
